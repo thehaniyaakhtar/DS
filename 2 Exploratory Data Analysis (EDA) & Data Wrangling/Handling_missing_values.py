a0e270f79f6db6973517a5b355b6655c4a3bbbf2
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-14 21:42:57 +0530

    Document missing value types in handling script
    Add explanations for missing value types in EDA.

--- 2 Exploratory Data Analysis (EDA) & Data Wrangling/Handling_missing_values.py
+++ 2 Exploratory Data Analysis (EDA) & Data Wrangling/Handling_missing_values.py
@@ -1,1 +1,12 @@
+""" 
+Why is a value missing:
+MCAR: Missing Completely at Random
+Missingness unrelated to any variable
 
+MAR: Missing At Random
+Missingness related to other observed variables
+
+MNAR: Missing Not At Random
+Missingness depends on the missing value itself
+Requires deeper modeling
+"""
