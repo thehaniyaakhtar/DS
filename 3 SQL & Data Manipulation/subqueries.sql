bbf3af780b38f56f3c539647adee8b3fe49168de
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 22:30:28 +0530

    Update subqueries.sql

--- 3 SQL & Data Manipulation/subqueries.sql
+++ 3 SQL & Data Manipulation/subqueries.sql
@@ -5,3 +5,18 @@ WHERE customer_id IN (
 	SELECT customer_id
 	FROM Orders
 );
+-- Customers who spent above average
+SELECT *
+FROM (
+  SELECT customer_id, SUM(amount) AS total
+  FROM Orders
+  GROUP BY customer_id
+) t
+WHERE total > (
+  SELECT AVG(total)
+  FROM(
+    SELECT SUM(amount) AS total
+    FROM Orders
+	GROUP BY customer_id
+  ) x
+);
