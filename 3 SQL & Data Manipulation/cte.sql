40aba15969fd0f227e5590c0294559dbac0fc5af
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 22:53:58 +0530

    Update cte.sql

--- 3 SQL & Data Manipulation/cte.sql
+++ 3 SQL & Data Manipulation/cte.sql
@@ -1,1 +1,12 @@
-
+-- common table expression
+-- temporary result set in SQL that can be referenced within a single query
+WITH customer_spending AS (
+  SELECT customer_id, SUM(amount) AS total
+  FROM Orders
+  GROUP BY customer_id
+)
+SELECT * 
+FROM customer_spending
+WHERE total > (
+  SELECT AVG(total) FROM customer_spending
+);
