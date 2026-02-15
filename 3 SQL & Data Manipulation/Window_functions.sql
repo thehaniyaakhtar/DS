b26399fb6167dd3f741b3ddc7ca9c44ff4d823df
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 21:54:58 +0530

    Update Window_functions.sql

--- 3 SQL & Data Manipulation/Window_functions.sql
+++ 3 SQL & Data Manipulation/Window_functions.sql
@@ -1,1 +1,14 @@
+-- Window Function: caalculates a value using a "window" of rows around the current row
 
+-- Row Number
+-- Ranking orders per customer
+SELECT customer_id,
+item,
+amount,
+ROW_NUMBER() OVER(
+  PARTITION BY customer_id
+  ORDER BY amount DESC
+)
+AS row_num
+FROM Orders;
+-- gives each customer's order a rank
