f35cd79da7a762f5c55af897443dd3618709995f
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 09:58:30 +0530

    Implement Z-Score method for outlier detection
    Added Z-Score method for outlier detection in data.

--- 2 Exploratory Data Analysis (EDA) & Data Wrangling/Outlier detection_&_treatment.py
+++ 2 Exploratory Data Analysis (EDA) & Data Wrangling/Outlier detection_&_treatment.py
@@ -5,3 +5,24 @@ the most important signals
 
 They are context dependent
 '''
+'''
+Z-Score Method
+Method assumes approximate normal distribution
+Each value is standardized
+Z score tells how many SDs away a point is away from the mean
+|Z| > 3, Potential Outlier, 99.7% data lies within +-3 SD
+'''
+import numpy as np
+
+mean = np.mean(X)
+std = np.std(X)
+
+x_scores = (X - mean) / std
+outliters = np.abs(z_scores) > 3
+
+'''
+used best when
+data roughly normal
+no heavy skew
+few extreme points
+'''
