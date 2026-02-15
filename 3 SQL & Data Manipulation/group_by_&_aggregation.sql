15d4121c8bfca0a3146c7781440163766d648764
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 19:47:26 +0530

    Update group_by_&_aggregation.sql

--- 3 SQL & Data Manipulation/group_by_&_aggregation.sql
+++ 3 SQL & Data Manipulation/group_by_&_aggregation.sql
@@ -8,3 +8,17 @@ LEFT JOIN Orders o
 ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.first_name;
 
+-- Count order per country
+SELECT 
+	c.country,
+COUNT(o.order_id) AS total_orders
+FROM Customers c
+LEFT JOIN Orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.country
+;
+
+
+
+
+
