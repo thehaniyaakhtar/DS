1ba10c10fac8c605011ef62bc0eec536909dad9f
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 11:44:59 +0530

    Document feature engineering methods in Feature_engineering.py
    Added feature engineering notes on scaling and normalization techniques.

--- 2 Exploratory Data Analysis (EDA) & Data Wrangling/Feature_engineering.py
+++ 2 Exploratory Data Analysis (EDA) & Data Wrangling/Feature_engineering.py
@@ -1,1 +1,31 @@
+'''
+Feature Engineering
+Transforming raw variables into representations that make patterns easier for models to learn
+
+Scaling and Normalization
+magnitude control
+
+Scaling matters:
+age: 0-100
+income: 0-1000000
+
+Distance based models will treat income far more imp because of scale
+'''
+'''
+Standardization(Z score scaling)
+x′= x−μ/σ
+mean = 0
+std = 1
+'''
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# causes features to be centered and scaled, optimization behaves better
+
+
+
+
+
 
