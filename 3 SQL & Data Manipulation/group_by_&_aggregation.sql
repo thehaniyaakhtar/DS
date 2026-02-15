7f2d4243dbe43c57602544aa24f5d47195aabb20
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 19:35:46 +0530

    Update group_by_&_aggregation.sql

--- 3 SQL & Data Manipulation/group_by_&_aggregation.sql
+++ 3 SQL & Data Manipulation/group_by_&_aggregation.sql
@@ -1,1 +1,10 @@
+-- Group By and Aggregation
+SELECT 
+	c.customer_id,
+	c.first_name,
+SUM(o.amount) AS total_spent
+FROM Customers c
+LEFT JOIN Orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.first_name;
 
