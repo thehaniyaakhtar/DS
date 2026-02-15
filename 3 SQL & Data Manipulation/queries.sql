db46750850636aec95774d00a6b2b9b8162a06a0
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 19:19:51 +0530

    Add inner join query for customers and orders
    Added SQL query to retrieve customers who have placed orders using inner join.

--- 3 SQL & Data Manipulation/queries.sql
+++ 3 SQL & Data Manipulation/queries.sql
@@ -1,1 +1,15 @@
+/*
+Joins
+Inner Joins
+*/
+-- customers who have placed orders:
+SELECT 
+	c.customer_id,
+	c.first_name,
+	o.order_id,
+	o.item,
+	o.amount
+FROM Customers c
+INNER JOIN Orders o
+	ON c.customer_id = o.customer_id;
 
