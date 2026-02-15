47d27cb4385514a1d56513091f46a571d1ccf542
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-15 16:10:33 +0530

    Update Handling_imbalanced_data.py

--- 2 Exploratory Data Analysis (EDA) & Data Wrangling/Handling_imbalanced_data.py
+++ 2 Exploratory Data Analysis (EDA) & Data Wrangling/Handling_imbalanced_data.py
@@ -1,1 +1,26 @@
+'''
+Imbalanced data 
+occurs when class distribution is skewed
+Fraud Detection:
+99.5%: Not Fraud
+0.5%: Fraud
+Model can predict "Not Fraud" always and get 99.5% accuracy, useless
 
+Undersampling:
+Reduce majority class size
+
+Original:
+10,000 majority
+500 minority
+
+After undersampling:
+500 majority
+500 minority
+
+it is simple, fast
+it throws aways data, can remove useful signal, risk of underfittig
+'''
+from imblearn.under_sampling import RandomUnderSampler
+
+rus = RandomUnderSampler()
+X_res, y_res = rus.fit_resample(X, y)
