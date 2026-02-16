54069b2603158b8369d63636e8e334e374654443
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-16 22:41:46 +0530

    Update data_exploration.py

--- ML/data_exploration.py
+++ ML/data_exploration.py
@@ -1,1 +1,31 @@
+#loading data
+df = pd.read_csv('file_path')
 
+#understanding data
+#shows no. of rows and columns, names of cols, data type, null count
+df.info()
+
+#understand data distribution numerically
+df.describe()
+
+#returns the no of rows and columns in a df
+df.shape
+
+# first/last 5/n rows 
+df.head()
+df.tail(n)
+
+# returns column names
+df.columns
+
+# unique values
+df.nunique()
+
+# number of null values in each col
+df.isnull().sum()
+
+# unique values in a column
+df['column'].value_counts()
+
+# correlation matrix from -1 to 1
+df.corr()
