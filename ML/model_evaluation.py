058bc350f1775fd14eb09ad79cd04d28616431c7
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-18 15:14:37 +0530

    Update model_evaluation.py

--- ML/model_evaluation.py
+++ ML/model_evaluation.py
@@ -11,3 +11,17 @@ optimal max_leaf_nodes: minimize MAE without overfitting
 
 need low mae as mae is the average of all your models mistakes
 '''
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+def get_mae(max_leaf_nodes, train_X, train_y, val_X, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes, random_state = 0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return mae
+    
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, train_y, val_X, val_y)
+    print(max_leaf_nodes, my_mae)
