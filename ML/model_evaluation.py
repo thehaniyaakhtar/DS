a5f8059433f0d16fa6a5cae315d06f30a994e1dc
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-18 14:58:49 +0530

    Update model_evaluation.py

--- ML/model_evaluation.py
+++ ML/model_evaluation.py
@@ -1,1 +1,11 @@
+# underfitting and overfitting
+'''
+Testing different values of max_leaf_nodes for Decision Tree and calculating MAE for each
+1. Train DTR with given max_leaf_nodes
+2. Making predictions on validation sets
+3. Calculates and prints MAE to evaluate performances
 
+low max_leaf_nodes = 5, high MAE, underfitting
+high max_leaf_nodes = 5000, overfit 
+optimal max_leaf_nodes: minimize MAE without overfitting
+'''
