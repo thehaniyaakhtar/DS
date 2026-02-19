c15b117e0f9d824370291d2745f5bdef4d55a489
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-19 14:36:28 +0530

    Update scoring_dataset.py

--- ML/scoring_dataset.py
+++ ML/scoring_dataset.py
@@ -1,1 +1,27 @@
+# scoring a dataset using a model
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=10, random_state=0)
+    # uses 10 decision trees
+    # random_state = 0 ensures reproducibility
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
 
+# identifying columns with missing values
+cols_missing_values = [
+    col for col in X_train.columns
+    if X_train[col].isnull().any()
+]
+
+# dropping columns with missing values
+reduced_X_train = X_train.drop(cols_missing_values, axis = 1)
+reduced_X_valid = V_valid.drop(cols_missing_values, axis = 1)
+
+# scoring after dropping columns
+print(score_dataset(
+    reduced_X_train,
+    reduced_X_valid,
+    y_train,
+    y_valid
+))
+# Train model without columns containing missing values
