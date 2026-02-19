efbdbff2c86cec92a4a8ec93761ea93aa07915bd
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-19 13:12:42 +0530

    Update parsing_dates.py

--- ML/parsing_dates.py
+++ ML/parsing_dates.py
@@ -1,1 +1,30 @@
+# working with data 
+# dates are stored as strings
+# they must be parsed into datetime format
+
+# checking data type
+df['date'].dtype
+
+# convert to datetime
+df['parsed'] = pd.to_datetime(df['date'])
+
+#specific format faster, more accurate
+df['parsed'] = pd.to_datetime(
+    df['date'],
+    format = '%m/%d/%y'
+)
+
+# handling multiple data formats
+df['date_parsed'] = pd.to_datetime(
+    df['date'],
+    infer_datetime_format = True
+)
+# pandas guesses the date format
+# it is slower, not always accurate
+
+# extracting info from dates
+day_of_month = df['data_parsed'].dt.day
+month = df['date_parsed'].dt.month
+year = df['date_parsed'].dt.year
+day_of_week = df['date_parsed'].dt.dayofweek 
 
