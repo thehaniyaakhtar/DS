0c802796d4c85214e0505cb8dd46d516791ea674
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-19 11:10:48 +0530

    Update scaling_&_normalization.py

--- ML/scaling_&_normalization.py
+++ ML/scaling_&_normalization.py
@@ -13,7 +13,7 @@
 # sensitive to outliers
 # relative distances between points remain same
 # sensitive to outliers
-data = pd.DataFrame(kickstarter, usecols = ['specific_col'])
+data = kickstarter[['specific_col']]
 scaled_data = minimax_scaling(data, columns=['specific_col'])
 # converts values range to [0, 1]
 
