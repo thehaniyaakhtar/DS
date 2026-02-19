f1fead91f40284a50989666a23064007f693a771
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-19 10:42:54 +0530

    Update scaling_&_normalization.py

--- ML/scaling_&_normalization.py
+++ ML/scaling_&_normalization.py
@@ -5,3 +5,11 @@
 # eg: [0, 10000] to [0, 1]
 # important for distance based algos, SVM, KNN
 # as larger features may end up dominating model
+
+# min max scaling
+# rescales data to a fixed range usually [0, 1]
+# preserves the original distribution shape
+# min value: 0, max value: 1
+# sensitive to outliers
+# relative distances between points remain same
+# sensitive to outliers
