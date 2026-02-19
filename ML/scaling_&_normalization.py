f6480cb88bba76b5f7d13e3d8f568c0d43c5f5a0
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-19 10:38:35 +0530

    Update scaling_&_normalization.py

--- ML/scaling_&_normalization.py
+++ ML/scaling_&_normalization.py
@@ -1,1 +1,7 @@
-
+# scaling and normalization
+#  scaling: changing the range of features values
+# data distribution shape/ relationship does not change
+# chnges the magnitude
+# eg: [0, 10000] to [0, 1]
+# important for distance based algos, SVM, KNN
+# as larger features may end up dominating model
