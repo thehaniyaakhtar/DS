a22d6e9557094012be1cefa0e3f1dc2836c0d1e4
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-19 06:39:51 +0530

    Update data_cleaning.py

--- ML/data_cleaning.py
+++ ML/data_cleaning.py
@@ -1,1 +1,15 @@
+import numpy as np
+
+# Missing per column
+missing_values = df.isnull().sum()
+count[0:10]
+
+# Total cells
+total = np.prod(df.shape)
+
+# Total missing values
+missing = missing_values.sum()
+
+# Percentage missing
+percent = (missing/total)*100
 
