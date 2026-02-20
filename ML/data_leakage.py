e71227be9c57b011dba712d889e35bfe390ae91b
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-20 13:18:56 +0530

    Update data_leakage.py

--- ML/data_leakage.py
+++ ML/data_leakage.py
@@ -6,3 +6,12 @@
 # eg: if someone has taken an antibiotic, it must mean they might have pneumonia
 # Train Test Contamination
 # Happens when validation data is accidentally used during training 
+
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+my_pipeline = make_pipeline(RandomForestClassifier(n_estimators=100))
+cv_score = cross_val_score(my_pipeline, X, y, cv = 5, scoring = 'accuracy')
+
+print(cv_score.mean())
