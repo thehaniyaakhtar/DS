fb57e69a700b1d201e52738b5e6d37ebb0824118
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-20 10:13:04 +0530

    Update pipelines.py

--- ML/pipelines.py
+++ ML/pipelines.py
@@ -1,1 +1,8 @@
+# pipelines
+# a pipeline automates preprocessing + modeling into one object
+# a pipeline bundles everything togetehr instead of doing it manually
+# imputation,  encoding, scaling, modeling
+# they prevent data leakage
+# reproducible and less manual mistakes
+# makes ml production ready
 
