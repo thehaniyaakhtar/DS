57e97e6396fe5c966d6e5496cd716138a652b9aa
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-20 13:06:30 +0530

    Update data_leakage.py

--- ML/data_leakage.py
+++ ML/data_leakage.py
@@ -1,1 +1,8 @@
-
+# Data Leakage
+# Training dataset is used to create a model
+# same dataset is not going to be available when the model is used 
+# Target Leakage
+# future data (not in test dataset) is included in training data
+# eg: if someone has taken an antibiotic, it must mean they might have pneumonia
+# Train Test Contamination
+# Happens when validation data is accidentally used during training 
