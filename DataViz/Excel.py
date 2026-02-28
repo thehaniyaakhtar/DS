22aaf338f6ba572b3831ce085f6377528762c9f7
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-02-28 14:59:14 +0530

    Update Excel.py

--- DataViz/Excel.py
+++ DataViz/Excel.py
@@ -1,1 +1,14 @@
+'''
+Data processing machine
+'''
+'''
+Cells, Ranges, Data Types
+Stores text in:
+Text, Nos, Dates, Boolean, Errors
 
+Excel treats dates as numbers
+01 becomes 1
+Some numeric columns behave like text
+This is because Excel infers type automatically, 
+this can corrupt raw datasets if not controlled.
+'''
